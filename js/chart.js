b3ae3646d7841dced68ffea2e24fa2e655fefa91
Richard Westenra <rjwestenra@gmail.com>
2015-10-08 18:57:42 +0100

    Add another force chart

--- js/chart.js
+++ js/chart.js
@@ -451,6 +451,7 @@
       .linkDistance(60)
       .charge(-300)
       .on('tick', tick)
+      .start();
 
     var svg = d3.select('#force')
       .attr('width', width)
@@ -476,51 +477,144 @@
       return 'translate(' + d.x + ',' + d.y + ')';
     }
 
-    this.start = function (){
-      force.start();
-      svg.selectAll('*').remove();
+    svg.selectAll('*').remove();
 
-      // Per-type markers, as they don't inherit styles.
-      svg.append('defs')
-        .selectAll('marker')
-        .data(['suit', 'licensing', 'resolved'])
-        .enter()
-        .append('marker')
-        .attr('id', function(d) { return d; })
-        .attr('viewBox', '0 -5 10 10')
-        .attr('refX', 15)
-        .attr('refY', -1.5)
-        .attr('markerWidth', 6)
-        .attr('markerHeight', 6)
-        .attr('orient', 'auto')
-        .append('path')
-        .attr('d', 'M0,-5L10,0L0,5');
+    // Per-type markers, as they don't inherit styles.
+    svg.append('defs')
+      .selectAll('marker')
+      .data(['suit', 'licensing', 'resolved'])
+      .enter()
+      .append('marker')
+      .attr('id', function(d) { return d; })
+      .attr('viewBox', '0 -5 10 10')
+      .attr('refX', 15)
+      .attr('refY', -1.5)
+      .attr('markerWidth', 6)
+      .attr('markerHeight', 6)
+      .attr('orient', 'auto')
+      .append('path')
+      .attr('d', 'M0,-5L10,0L0,5');
 
-      path = svg.append('g')
-        .selectAll('path')
-        .data(force.links())
-        .enter()
-        .append('path')
-        .attr('class', function(d) { return 'link ' + d.type; })
-        .attr('marker-end', function(d) { return 'url(#' + d.type + ')'; });
+    path = svg.append('g')
+      .selectAll('path')
+      .data(force.links())
+      .enter()
+      .append('path')
+      .attr('class', function(d) { return 'link ' + d.type; })
+      .attr('marker-end', function(d) { return 'url(#' + d.type + ')'; });
 
-      circle = svg.append('g')
-        .selectAll('circle')
-        .data(force.nodes())
-        .enter()
-        .append('circle')
-        .attr('r', 6)
-        .call(force.drag);
+    circle = svg.append('g')
+      .selectAll('circle')
+      .data(force.nodes())
+      .enter()
+      .append('circle')
+      .attr('r', 6)
+      .call(force.drag);
 
-      text = svg.append('g')
-        .selectAll('text')
-        .data(force.nodes())
-        .enter()
-        .append('text')
-        .attr('x', 8)
-        .attr('y', '.31em')
-        .text(function(d) { return d.name; });
-    };
+    text = svg.append('g')
+      .selectAll('text')
+      .data(force.nodes())
+      .enter()
+      .append('text')
+      .attr('x', 8)
+      .attr('y', '.31em')
+      .text(function(d) { return d.name; });
+  }
+
+
+
+
+  function ForceChart2(){
+
+    var width = 600,
+      height = 400;
+
+    var nodes = d3.range(200).map(function() { return {radius: Math.random() * 7 + 3}; }),
+      root = nodes[0],
+      color = d3.scale.category10();
+
+    root.radius = 0;
+    root.fixed = true;
+
+    var force = d3.layout.force()
+      .gravity(0.05)
+      .charge(function(d, i) { return i ? 0 : -300; })
+      .nodes(nodes)
+      .size([width, height]);
+
+    force.start();
+
+    var svg = d3.select('#force2')
+      .attr('width', width)
+      .attr('height', height);
+
+    svg.selectAll('*').remove();
+
+    svg.selectAll('circle')
+      .data(nodes.slice(1))
+      .enter().append('circle')
+      .attr('r', function(d) { return d.radius; })
+      .style('fill', function(d, i) { return color(i % 3); });
+
+    force.on('tick', function(e) {
+      var q = d3.geom.quadtree(nodes),
+        i = 0,
+        n = nodes.length;
+
+      while (++i < n) q.visit(collide(nodes[i]));
+
+      svg.selectAll('circle')
+        .attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; });
+    });
+
+    svg.on('mousemove', function() {
+      var p1 = d3.mouse(this);
+      root.px = p1[0];
+      root.py = p1[1];
+      force.resume();
+    });
+
+
+    d3.select('#gravity').on('change',function (){
+      force.gravity(this.value).start();
+      d3.select('#gravityVal').text(this.value);
+    });
+
+    d3.select('#charge').on('change',function (){
+      force.charge(function(d, i) { return i ? this.value : -500; }.bind(this))
+        .start();
+      d3.select('#chargeVal').text(this.value);
+    });
+
+    d3.select('#friction').on('change',function (){
+      force.friction(this.value).start();
+      d3.select('#frictionVal').text(this.value);
+    });
+
+    function collide(node) {
+      var r = node.radius + 16,
+      nx1 = node.x - r,
+      nx2 = node.x + r,
+      ny1 = node.y - r,
+      ny2 = node.y + r;
+      return function(quad, x1, y1, x2, y2) {
+        if (quad.point && (quad.point !== node)) {
+          var x = node.x - quad.point.x,
+            y = node.y - quad.point.y,
+            l = Math.sqrt(x * x + y * y),
+            r = node.radius + quad.point.radius;
+          if (l < r) {
+            l = (l - r) / l * .5;
+            node.x -= x *= l;
+            node.y -= y *= l;
+            quad.point.x += x;
+            quad.point.y += y;
+          }
+        }
+        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+      };
+    }
   }
 
 
@@ -551,7 +645,7 @@
   var tc = new TransChart();
   var sc = new ScaleChart();
   var gc = new GeoChart();
-  var fc = new ForceChart();
+  // var fc = new ForceChart();
 
   startInterval(getNewAdjective, 500);
 
@@ -581,7 +675,11 @@
         break;
       case 'force':
         setTimeout(function(){
-          fc.start();
+          new ForceChart();
+        }, 600);
+      case 'force2':
+        setTimeout(function(){
+          new ForceChart2();
         }, 600);
         break;
     }
